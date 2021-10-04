0048d356eab2b2b10a5ae30fc8af73a320b5a4ab
jessicamcg <jessicamcguico@gmail.com>
2021-10-04 12:14:05 -0500

    submit locations functionality

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,35 @@ fetch("https://unsa-unofficial-spotify-api.p.rapidapi.com/search?query=" + query
 .catch(err => {
 	console.error(err);
 });
+
+
+
+var locationSubmitBtn = $('#location-submit-btn');
+var locationStartInput = $('#start');
+var locationEndInput= $('#end');
+
+function handleLocationSubmitBtn(event) {
+	event.preventDefault();
+	console.log('click');
+	var locationStart = locationStartInput.val().trim();
+	var locationEnd =locationEndInput.val().trim();
+	console.log(locationStart);
+	console.log(locationEnd);
+	if (locationStart && locationEnd) {
+		getTravelDuration(locationStart,locationEnd);
+		getPlaylistForm();
+	} else {
+		alert('Please fill out both locations')
+	};
+};
+
+function getTravelDuration(start,end) {
+	console.log(start);
+	console.log(end);
+};
+
+function getPlaylistForm() {
+	console.log('test');
+};
+
+locationSubmitBtn.on('click',handleLocationSubmitBtn);
